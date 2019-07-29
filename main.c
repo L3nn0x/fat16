d2ffee11cb6eb8d29d73c87a7a7a6c22e09dc12d
L3nn0x <dragon83.super@gmail.com>
2019-07-29 14:55:50 +0100

    Update main.c

--- main.c
+++ main.c
@@ -9,12 +9,28 @@ int main(int argc, char* argv[]) {
         printf("Error, missing argument. Usage %s <filename>\n", argv[0]);
         return 1;
     }
-
-    if (fork()) {
-        execl("ls/ls", "ls/ls", argv[1]);
-    } else {
 	init(argv[1]);
-        deinit();
+    
+    char path[256] = "/";
+    char input[256];
+    
+    while (1) {
+        printf("%s > ", path);
+        if (!fgets(input, sizeof(input), stdin)) {
+            printf("\n");
+            break;
+        }
+        if (!strcmp(input, "ls\n")) {
+            if (!fork()) {
+                execl("ls/ls", "ls/ls", argv[1], path, 0);
+            } else {
+                wait(0);
+            }
+        } else if (!strcmp(input, "exit\n")) {
+            break;
+        }
     }
-	return 0;
+
+    deinit();
+    return 0;
 }
